2e712af9f0eb1acb22806cba6f1459a5abe50d9e
krishkakadiya12 <krishkakadiya102@gmail.com>
2024-02-20 13:29:55 +0530

    v2

--- src/layout/Mainlayout.tsx
+++ src/layout/Mainlayout.tsx
@@ -1,23 +1,22 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode } from "react";
 import Nav from "../components/Nav";
 import Sidebar from "../components/Sidebar";
 import "../css/Mainlayout.css"; // Import your CSS file for styling
 
 interface MyLayoutProps {
-    children: ReactNode;
+  children: ReactNode;
 }
 
 const MyLayout: React.FC<MyLayoutProps> = ({ children }) => {
-    return (
-        <div className="my-layout-container"> 
-            <Nav />
-            <Sidebar />
-            <main className="my-layout-main" > 
-                {children}
-            </main>
-
-        </div>
-    );
-}
+  return (
+    <div className="my-layout-container">
+      <Nav />
+      <div className="middle">
+        <Sidebar />
+        <main className="my-layout-main">{children}</main>
+      </div>
+    </div>
+  );
+};
 
 export default MyLayout;
