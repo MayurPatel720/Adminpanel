c7abfdf48598b8822c12a6361041d44860f43c82
MayurPatel720 <mayurpatell7020@gmail.com>
2024-10-06 21:07:37 +0530

    quiz users

--- src/components/AllFeed.tsx
+++ src/components/AllFeed.tsx
@@ -17,6 +17,13 @@ import {
 } from "../queries/allda";
 import EmptyView from "./EmptyView";
 import Loading from "./Loading";
+import {
+  Paginator,
+  PaginatorCurrentPageReportOptions,
+  PaginatorPageChangeEvent,
+  PaginatorRowsPerPageDropdownOptions,
+} from "primereact/paginator";
+import { Dropdown } from "primereact/dropdown";
 
 interface FeedItem {
   _id: string;
@@ -218,6 +225,56 @@ const AllFeed = () => {
         });
       },
     });
+  };
+
+  const template2 = {
+    layout: "RowsPerPageDropdown CurrentPageReport PrevPageLink NextPageLink",
+    RowsPerPageDropdown: (options: PaginatorRowsPerPageDropdownOptions) => {
+      const dropdownOptions = [
+        { label: 5, value: 5 },
+        { label: 10, value: 10 },
+        { label: 20, value: 20 },
+        { label: 120, value: 120 },
+      ];
+
+      return (
+        <React.Fragment>
+          <span
+            className="mx-1"
+            style={{ color: "var(--text-color)", userSelect: "none" }}
+          >
+            Items per page:{" "}
+          </span>
+          <Dropdown
+            value={options.value}
+            options={dropdownOptions}
+            onChange={options.onChange}
+          />
+        </React.Fragment>
+      );
+    },
+    CurrentPageReport: (options: PaginatorCurrentPageReportOptions) => {
+      return (
+        <span
+          style={{
+            color: "var(--text-color)",
+            userSelect: "none",
+            width: "120px",
+            textAlign: "center",
+          }}
+        >
+          {options.first} - {options.last} of {options.totalRecords}
+        </span>
+      );
+    },
+  };
+
+  const [first, setFirst] = useState<number[]>([0, 0, 0]);
+  const [rows, setRows] = useState([10, 10, 10]);
+
+  const onPageChange = (e: PaginatorPageChangeEvent, index: number) => {
+    setFirst(first.map((f, i) => (index === i ? e.first : f)));
+    setRows(rows.map((r, i) => (index === i ? e.rows : r)));
   };
 
   return (
@@ -533,6 +590,14 @@ const AllFeed = () => {
           )}
         </div>
       )}
+      <Paginator
+        template={template2}
+        first={first[1]}
+        rows={rows[1]}
+        totalRecords={120}
+        onPageChange={(e) => onPageChange(e, 1)}
+        className="justify-content-end"
+      />
     </Mainlayout>
   );
 };
